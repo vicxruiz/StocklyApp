b9b6c82522c97c0cbe7470e26369aadbe4f65d0d
Victor Ruiz <31416135+vicxruiz@users.noreply.github.com>
2023-07-20 20:01:37 -0400

    init

---
+++ StocklyiOS/StocklyiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocklyiOSApp.swift
+//  StocklyiOS
+//
+//  Created by Victor Ruiz on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct StocklyiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
