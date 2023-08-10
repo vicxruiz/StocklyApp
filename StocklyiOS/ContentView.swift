51633b7b32b7ec676419a3ddee75ba8b1cb4523d
Victor Ruiz <31416135+vicxruiz@users.noreply.github.com>
2023-08-10 14:39:09 -0400

    fixing query

--- StocklyiOS/ContentView.swift
+++ StocklyiOS/ContentView.swift
@@ -18,7 +18,6 @@ struct ContentView: View {
     @State private var hasAppeared = false
     @ObservedObject var viewModel = ContentViewModel(repository: StockService())
     @State var addSymbolPressed: Bool = false
-    @Environment(\.isSearching) private var isSearching
     
     var body: some View {
         NavigationStack {
@@ -34,7 +33,7 @@ struct ContentView: View {
                     .offset(y: -40)
                 } else {
                     List {
-                        Section("My Symbols") {
+                        Section("My Watch List") {
                             ForEach(viewModel.content.indices, id: \.self) { index in
                                 NavigationLink {
                                     Text("\(index)")
@@ -51,7 +50,6 @@ struct ContentView: View {
             .task {
                 guard !hasAppeared else { return }
                 hasAppeared = true
-                viewModel.trigger(.fetchStocks)
             }
             .navigationTitle("Stocks")
         }
@@ -64,9 +62,9 @@ struct StockCell: View {
     var body: some View {
         HStack {
             Button {
-                viewModel.content[index].isStored.toggle()
+                //viewModel.content[index].isStored.toggle()
             } label: {
-                if viewModel.content[index].isStored {
+                if false {
                     Image(systemName: "checkmark.circle.fill")
                         .symbolRenderingMode(.hierarchical)
                         .foregroundColor(Color.teal)
@@ -78,20 +76,23 @@ struct StockCell: View {
             .buttonStyle(.plain)
             VStack(alignment: .leading, spacing: 4) {
                 HStack {
-                    Text("AAPL")
+                    Text(viewModel.content[index].symbol)
                         .fontWeight(.bold)
-                    Text("NYSE")
+                        .scaledToFit()
+                    Text(viewModel.content[index].exchange)
                         .foregroundColor(Color(uiColor: .darkGray))
                         .font(.footnote)
                         .fontWeight(.semibold)
-                    Text("USD")
+                    Text(viewModel.content[index].currency)
                         .foregroundColor(Color(uiColor: .darkGray))
                         .font(.footnote)
                         .fontWeight(.semibold)
                 }
-                Text("Apple Inc.")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
+                if !viewModel.content[index].name.isEmpty {
+                    Text(viewModel.content[index].name)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
             }
             Spacer()
             VStack(alignment: .trailing, spacing: 6) {
@@ -108,19 +109,33 @@ struct StockCell: View {
                     .cornerRadius(6)
             }
         }
+        .task {
+            await viewModel.trigger(.fetchPrice(viewModel.content[index].symbol))
+        }
     }
 }
 
-final class ContentViewModel: ObservableObject {
+final class ContentViewModel: NSObject, ObservableObject {
     @Published var query: String = "" {
         didSet {
-            print(query)
+            NSObject.cancelPreviousPerformRequests(
+                withTarget: self,
+                selector: #selector(querySymbol),
+                object: self
+            )
+            
+            guard !query.isEmpty else {
+                return
+            }
+            
+            self.perform(#selector(querySymbol), with: nil, afterDelay: 0.5)
         }
     }
-    @Published var content: [Content] = []
+    @Published var content: [SearchResponse] = []
     
     enum Input {
         case fetchStocks
+        case fetchPrice(String)
     }
     
     let repository: StockRepository
@@ -129,9 +144,33 @@ final class ContentViewModel: ObservableObject {
         self.repository = repository
     }
     
-    func trigger(_ input: Input) {
+    func trigger(_ input: Input) async {
         switch input {
         case .fetchStocks: fetchStocks()
+        case .fetchPrice(let symbol): await fetchStockPrice(for: symbol)
+        }
+    }
+    
+    @objc private func querySymbol() {
+        repository.query(for: query) { result in
+            switch result {
+            case .success(let response):
+                DispatchQueue.main.async {
+                    self.content = response
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func fetchStockPrice(for symbol: String) async {
+        let response = await repository.fetchStockPrice(for: symbol)
+        switch response {
+        case .success(let success):
+            print(success)
+        case .failure(let failure):
+            print(failure)
         }
     }
     
@@ -147,25 +186,35 @@ enum StockError: Error {
 protocol StockRepository {
     func fetchStocks(completion: @escaping (Result<[Content], Error>)->Void)
     func query(for symbol: String, completion: @escaping (Result<[SearchResponse], Error>)->Void)
+    func fetchStockPrice(for symbol: String) async -> Result<(String, String), Error>
 }
 
 final class StockService: StockRepository {
     let baseURL = "https://api.twelvedata.com"
+    let apiKey = "017d616c69e24cb382a98257702a34a0"
     
     enum QueryItem: String {
-        case search = "search_symbol"
+        case symbol = "symbol"
+        case apiKey = "apikey"
+    }
+    
+    enum Path: String {
+        case search = "symbol_search"
+        case price = "price"
+        case quote = "quote"
     }
     
     func query(for symbol: String, completion: @escaping (Result<[SearchResponse], Error>) -> Void) {
         var components = URLComponents(string: baseURL)
-        let searchQueryItem = URLQueryItem(name: QueryItem.search.rawValue, value: symbol)
+        let symbolQueryItem = URLQueryItem(name: QueryItem.symbol.rawValue, value: symbol)
         
-        components?.queryItems = [searchQueryItem]
+        components?.queryItems = [symbolQueryItem]
         
-        guard let url = components?.url else {
+        guard var url = components?.url else {
             completion(.failure(StockError.invalidURL))
             return
         }
+        url.append(path: Path.search.rawValue)
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -188,6 +237,67 @@ final class StockService: StockRepository {
         task.resume()
     }
     
+    func fetchStockPrice(for symbol: String) async -> Result<(String, String), Error> {
+        do {
+            let change = try await fetchChange(for: symbol)
+            let realTimePrice = try await fetchRealTimePrice(for: symbol)
+            
+            // Update your UI with the fetched data
+            print("Historical Change: \(change)")
+            print("Real-Time Price: \(realTimePrice)")
+            
+            // Update your UI components with the fetched data
+            return .success((change, realTimePrice))
+        } catch {
+            print("Error: \(error)")
+            return .failure(error)
+        }
+    }
+    
+    // Fetch historical data
+    private func fetchChange(for symbol: String) async throws -> String {
+        var components = URLComponents(string: baseURL)
+        let symbolQueryItem = URLQueryItem(name: QueryItem.symbol.rawValue, value: symbol)
+        let apiQueryItem = URLQueryItem(name: QueryItem.apiKey.rawValue, value: apiKey)
+        
+        components?.queryItems = [symbolQueryItem, apiQueryItem]
+        
+        guard var url = components?.url else {
+            throw StockError.invalidURL
+        }
+        url.append(path: Path.quote.rawValue)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let decoder = JSONDecoder()
+        let stockData = try decoder.decode(QuoteResponse.self, from: data)
+        return stockData.change ?? "0"
+    }
+
+    // Fetch real-time price
+    private func fetchRealTimePrice(for symbol: String) async throws -> String {
+        var components = URLComponents(string: baseURL)
+        let symbolQueryItem = URLQueryItem(name: QueryItem.symbol.rawValue, value: symbol)
+        let apiQueryItem = URLQueryItem(name: QueryItem.apiKey.rawValue, value: apiKey)
+        
+        components?.queryItems = [symbolQueryItem, apiQueryItem]
+        
+        guard var url = components?.url else {
+            throw StockError.invalidURL
+        }
+        url.append(path: Path.price.rawValue)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        print(url)
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let decoder = JSONDecoder()
+        let stockData = try decoder.decode(PriceResponse.self, from: data)
+        return stockData.price ?? "0"
+    }
+    
     func fetchStocks(completion: @escaping (Result<[Content], Error>) -> Void) {
         print("fetched")
         var result: [Content] = []
@@ -196,6 +306,14 @@ final class StockService: StockRepository {
         }
         completion(.success(result))
     }
+}
+
+struct QuoteResponse: Codable {
+    let change: String?
+}
+
+struct PriceResponse: Codable {
+    let price: String?
 }
 
 struct SearchRootRespose: Codable {
